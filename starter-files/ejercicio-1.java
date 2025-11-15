2868a5f3f11d445188ecd76521a9f7714191ca5e
Matias Armestar <102616485+MatiasArmestar@users.noreply.github.com>
2025-11-15 16:43:54 -0500

    feat: add exercise 1 java file
    Add a simple Java program that prints messages.

---
+++ starter-files/ejercicio-1.java
@@ -0,0 +1,10 @@
+/*
+Cambiar el mensaje por su hobby o comida favorita.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo!");
+        System.out.println("Este es tu primer programa en Java :)");
+    }
+}

cf7bb5258b93ab9972490573845e94ea9233b70f
Matias Armestar <102616485+MatiasArmestar@users.noreply.github.com>
2025-11-15 16:35:05 -0500

    refactor: remove unused code
    Removed introductory comments and example code.

--- completed-examples/actividad-2.java
+++ completed-examples/actividad-2.java
@@ -1,16 +1,1 @@
-/*
 
-Programación Orientada a Objetos (POO) en Java desde cero
-
-Lección 2: Tu primer programa en Java
-
-*/
-
-public class Persona {
-  int edad = 25;
-
-  public static void main(String[] args) {
-    Persona myObj1 = new Persona();
-    System.out.println(myObj1.edad);
-  }
-}

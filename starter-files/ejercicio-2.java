9f48accc2122144dcfed847451147eed9bcab3a9
Matias Armestar <102616485+MatiasArmestar@users.noreply.github.com>
2025-11-15 16:45:45 -0500

    feat: add exercise 2 java file

---
+++ starter-files/ejercicio-2.java
@@ -0,0 +1,18 @@
+/*
+Mostrar un mensaje saludando haciendo uso de variables: "Hola, soy [nombres y apellidos], tengo [edad] años y me gusta [hobby]".
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        int edad = 14;
+        double altura = 1.65;
+        String nombre = "Ana";
+        boolean leGustaProgramar = true;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("¿Le gusta programar?: " + leGustaProgramar);
+    }
+}
